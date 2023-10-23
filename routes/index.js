40a75115ce714d862f51e5d01bd106e15c51a90c
stpafk <stephan.weigert@hotmail.com>
2023-10-23 09:09:10 -0300

    change index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+const messages = [
+  {
+    text: "Hello",
+    user: "Jane Doe",
+    added: new Date(),
+  },
+  {
+    text: "Hello",
+    user: "Erick Bambino",
+    added: new Date(),
+  }
+]
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
